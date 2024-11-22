6d23c6d0605405101be50ac38fd043a119a079aa
Arjan Draisma <arjan.draisma@wur.nl>
2024-11-22 11:39:46 +0100

    add static file view for precalculated results

--- websmash/default_settings.py
+++ websmash/default_settings.py
@@ -1,9 +1,10 @@
 from os import path
+
 ############# Configuration #############
 DEBUG = True
 SECRET_KEY = "development_key"
-RESULTS_PATH = path.join(path.dirname(path.dirname(__file__)), 'results')
-RESULTS_URL = '/upload'
+RESULTS_PATH = path.join(path.dirname(path.dirname(__file__)), "results")
+RESULTS_URL = "/upload"
 
 # Flask-Mail settings
 MAIL_SERVER = "mail.example.org"
@@ -13,6 +14,8 @@ DEFAULT_RECIPIENTS = ["bob@example.org"]
 # Flask-Redis settings
 REDIS_URL = "redis://localhost:6379/0"
 
+# precalculated results location. for sure change this
+PRECALCULATED_RESULTS = "/precalc"
+
 OLD_JOB_COUNT = 0
 #########################################
-
--- websmash/views.py
+++ websmash/views.py
@@ -1,4 +1,4 @@
-from flask import redirect, url_for, request, abort, \
+from flask import redirect, send_file, url_for, request, abort, \
                   render_template, jsonify
 from flask.ext.mail import Message
 import os
@@ -7,6 +7,8 @@ from werkzeug import secure_filename
 from websmash import app, mail, get_db
 from websmash.utils import generate_confirmation_mail
 from websmash.models import Job, Notice
+
+import default_settings as settings
 
 def _submit_job(redis_store, job):
     """Submit a new job"""
@@ -211,6 +213,27 @@ def server_status():
                    long_running=long_running, total_jobs=total_jobs,
                    ts_queued=ts_queued, ts_queued_m=ts_queued_m,
                    ts_timeconsuming=ts_timeconsuming, ts_timeconsuming_m=ts_timeconsuming_m)
+
+@app.route('/precalc', defaults={'req_path': ''})
+@app.route('/precalc/<path:req_path>')
+def dir_listing(req_path):
+    BASE_DIR = settings.PRECALCULATED_RESULTS
+
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, req_path)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+        
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files, path=req_path)
 
 
 def _get_oldest_job(queue):
