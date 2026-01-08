fa4cdd01b4e0073c9c0a6e378427022d0589fee7
Arjan Draisma <arjan.draisma@wur.nl>
2026-01-08 14:31:25 +0100

    add js protection against arbitrary file upload

--- websmash/static/js/antismash.js
+++ websmash/static/js/antismash.js
@@ -172,6 +172,19 @@ function is_gff3(file) {
     return res;
 }
 
+function is_valid_coexpress(file) {
+    var ext = get_ext(file.toLowerCase())
+    var valid_exts = ['soft', 'csv']
+    var res = false;
+    for (var i in valid_exts) {
+        if (ext == valid_exts[i]) {
+            res = true;
+            break;
+        }
+    }
+    return res;
+}
+
 function getFileSize() {
     var input, file;
 
@@ -196,6 +209,7 @@ function verify_nucl_form() {
     var file = $('#seq').val();
     var ncbi = $('#ncbi').val();
     var gff = $('#gff').val();
+    var coexpress = $('#coexpress_file').val();
 
     if( (file === '' || file === null) && (ncbi === '' || ncbi === null)){
         alert('No input file provided. Please enter NCBI number or upload your own file');
@@ -207,6 +221,9 @@ function verify_nucl_form() {
         return false;
     } else if ( is_fasta(file) && !is_gff3(gff) ) {
         alert('Please provide annotation file (GFF3 format)');
+        return false;
+    } else if ( !is_valid_coexpress(coexpress) ) {
+        alert('Please provide coexpress file (SOFT or csv format)');
         return false;
     }
 
